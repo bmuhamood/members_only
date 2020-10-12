081e296cc3532140d7e083f4261465e514b029a6
balerum03 <adafer123.angra@gmail.com>
2020-10-12 14:55:43 -0500

    Added corresponding routes for Posts and Users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+    before_action :get_user
 
   # GET /posts
   # GET /posts.json
@@ -69,6 +70,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:author, :title, :body)
+      params.require(:post).permit(:username, :title, :body)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: [:new, :create, :index]
   devise_for :users
-  root to: "home#index"
+  root "posts#index"
 end
